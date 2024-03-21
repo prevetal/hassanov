505678f406e7db338bbd3dbe39c39babe7463bdd
Vitaliy Sulimenko <prevetal@gmail.com>
2024-03-21 12:11:39 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,157 @@
+WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+BODY = document.getElementsByTagName('body')[0]
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	// Input password
+	$('.form .toggle_btn').click(function(e) {
+		e.preventDefault()
+
+		let parent = $(this).closest('.line')
+
+		$(this).toggleClass('active')
+
+		$(this).hasClass('active')
+			? parent.find('.input').attr('type', 'text')
+			: parent.find('.input').attr('type', 'password')
+	})
+
+
+	// Phone input mask
+	const phoneInputs = document.querySelectorAll('input[type=tel]')
+
+	if (phoneInputs) {
+		phoneInputs.forEach(el => {
+			IMask(el, {
+				mask: '+{7} (000) 000-00-00',
+				lazy: true
+			})
+		})
+	}
+
+
+	// Focus when clicking on the field name
+	const formLabels = document.querySelectorAll('form .label')
+
+	if (formLabels) {
+		formLabels.forEach(el => {
+			el.addEventListener('click', e => {
+				e.preventDefault()
+
+				el.closest('.line').querySelector('.input, textarea').focus()
+			})
+		})
+	}
+
+
+	// Tests
+	$('.tests .test .head').click(function(e) {
+		e.preventDefault()
+
+		let parent = $(this).closest('.test')
+
+		parent.toggleClass('active')
+		parent.find('.data').slideToggle(300)
+	})
+
+
+	// Patients
+	$('.patients .patient .head').click(function(e) {
+		e.preventDefault()
+
+		let parent = $(this).closest('.patient')
+
+		parent.toggleClass('active')
+		parent.find('.data').slideToggle(300)
+	})
+
+
+	// Test info - nutrients - list
+	$('.test_item_info .nutrients .list .spoler_btn').click(function(e) {
+		e.preventDefault()
+
+		$(this).toggleClass('active').closest('.head').next().slideToggle(300)
+	})
+
+
+	// Tabs
+	var locationHash = window.location.hash
+
+	$('body').on('click', '.tabs .btn', function(e) {
+		e.preventDefault()
+
+		if (!$(this).hasClass('active')) {
+			let parent = $(this).closest('.tabs_container'),
+				activeTab = $(this).data('content'),
+				activeTabContent = $(activeTab),
+				level = $(this).data('level')
+
+			parent.find('.tabs:first .btn').removeClass('active')
+			parent.find('.tab_content.' + level).removeClass('active')
+
+			$(this).addClass('active')
+			activeTabContent.addClass('active')
+		}
+	})
+
+	if (locationHash && $('.tabs_container').length) {
+		let activeTab = $(`.tabs button[data-content="${locationHash}"]`),
+			activeTabContent = $(locationHash),
+			parent = activeTab.closest('.tabs_container'),
+			level = activeTab.data('level')
+
+		parent.find('.tabs:first .btn').removeClass('active')
+		parent.find('.tab_content.' + level).removeClass('active')
+
+		activeTab.addClass('active')
+		activeTabContent.addClass('active')
+
+		$('html, body').stop().animate({ scrollTop: $activeTabContent.offset().top }, 1000)
+	}
+
+
+	// Calendar
+	const dateInputs = document.querySelectorAll('.date_input')
+
+	if (dateInputs) {
+		dateInputs.forEach((el, i) => {
+			new AirDatepicker(el, {
+				autoClose: true,
+				maxDate: new Date(),
+				dateFormat: 'dd-MM-yyyy'
+			})
+		})
+	}
+})
+
+
+
+window.addEventListener('resize', function () {
+	WH = window.innerHeight || document.clientHeight || BODY.clientHeight
+
+	let windowW = window.outerWidth
+
+	if (typeof WW !== 'undefined' && WW != windowW) {
+		// Overwrite window width
+		WW = window.innerWidth || document.clientWidth || BODY.clientWidth
+
+
+		// Mob. version
+		if (!fakeResize) {
+			fakeResize = true
+			fakeResize2 = false
+
+			document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+		}
+
+		if (!fakeResize2) {
+			fakeResize2 = true
+
+			if (windowW < 375) document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
+		} else {
+			fakeResize = false
+			fakeResize2 = true
+		}
+	}
+})
